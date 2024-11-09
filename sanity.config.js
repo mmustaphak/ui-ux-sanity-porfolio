0bb481dade3e20ee41a4cb691da1ef7ceea104d9
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-11-09 10:38:35 +0530

    removed  visitonTool

--- sanity.config.js
+++ sanity.config.js
@@ -1,6 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
+// import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -10,7 +10,8 @@ export default defineConfig({
   projectId: 'p1zjsjf8',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  //visionTool() goes here
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
